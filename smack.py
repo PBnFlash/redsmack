491e9a4b0bd70f67068767ab7901c8942fbb4f65
PBnFlash <PBnFlash@gmail.com>
2020-05-24 19:15:59 -0700

    Update smack.py

--- smack.py
+++ smack.py
@@ -3,5 +3,5 @@ from redbot.core import commands
 class Smack(commands.Cog):
 
     @commands.command()
-    async def smackcom(self, ctx):
+    async def smack(self, ctx):
         await ctx.send("ow!")
