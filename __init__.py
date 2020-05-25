6cb80e4210b9a2804365cee8c3ba3340cfd5ebf3
PBnFlash <PBnFlash@gmail.com>
2020-05-24 18:59:03 -0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .smack import Smack
+
+def setup(bot):
+    bot.add_cog(Smack())
---
+++ smack.py
@@ -0,0 +1,7 @@
+from redbot.core import commands
+
+class Smack(commands.Cog):
+
+    @commands.command()
+    async def SMACK(self, ctx):
+        await ctx.send("ow!")
